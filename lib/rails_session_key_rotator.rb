c36c24c311591191c968edd23321c72aee775cd6
Steve Hodgkiss <steve@hodgkiss.me>
2014-08-04 16:00:54 +1000

    New gem

---
+++ lib/rails_session_key_rotator.rb
@@ -0,0 +1,5 @@
+require "rails_session_key_rotator/version"
+
+module RailsSessionKeyRotator
+  # Your code goes here...
+end
---
+++ lib/rails_session_key_rotator/version.rb
@@ -0,0 +1,3 @@
+module RailsSessionKeyRotator
+  VERSION = "0.0.1"
+end
